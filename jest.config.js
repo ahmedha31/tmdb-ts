20a776e19e1f45b256092f7acba19b60e8fbe297
Ahmed Hassen <aahhmmeeddhhaasseenn12332@gmail.com>
2025-03-03 06:39:46 +0200

    chore: enable coverage reporting and configure Codecov settings

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,23 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text', 'text-summary'],
   testMatch: ['**/test/**/*.test.ts'],
   collectCoverageFrom: [
     'src/**/*.ts',
-    '!src/index.ts',
-    '!src/types/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.ts',
+    '!src/**/index.ts',
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
+      branches: 70,
       functions: 80,
       lines: 80,
-      statements: 80
-    }
+      statements: 80,
+    },
   },
   verbose: true
 };
